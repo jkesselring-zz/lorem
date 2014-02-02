14349a997af78ef22f459f0be49a9079db50e519
jkesselring <joseph.kesselring@gmail.com>
2014-02-02 04:37:46 -0500

    Updated to demonstrate both html and 'raw' output.
    Hopefully this makes it easier to see exactly what output looks like.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 	require 'jkesselring/lorem/generate/generator.php';
 
-	$lorem = new JKesselring\Lorem\Generator('html', 10);
+	$lorem = new JKesselring\Lorem\Generator('html', 3);
 	$text = $lorem->generate();
-	echo $text;
+	
+	echo $text . "<br><hr><br>";
+	echo "<pre> $text </pre>";
